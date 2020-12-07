147234349217346556ab2c7ef84f37abaa7c3968
diva <martin.arce.teixeira@gmail.com>
2020-12-07 22:56:07 +0100

    A

--- generateSidenav.js
+++ generateSidenav.js
@@ -1,38 +1,89 @@
 export default function generateSidebarList() {
-    
-
-
 const slideOut = document.querySelector("#slide-out");
 
-let html12 = `
-<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-<li><a href="#!">CAGO A A</a></li>
-<li><a href="#!">Second Link</a></li>
-<li><a href="#!">Second Link</a></li>
+let lessons = [
+    {
+        level: 1,
+        lessonTitle: "1 Sustantivos",
+        lessonType: "explicacion",
+        id: "cmaiwedsfasdfsdf",
+        link: "sustantivos.html",
+        logo: "",
+        logoColor: ""
+    }
+    ,{
+        level: 1,
+        lessonTitle: "2 Sustantivos 2",
+        lessonType: "actividad",
+        id: "asdfasdfasdf",
+        link: "actividadsustantivos.html",
+        logo: "",
+        logoColor: "blue"
+    },
+    {
+        level: 2,
+        lessonTitle: "3 Adjetivos",
+        lessonType: "explicacion",
+        id: "asdfasdasfasdf",
+        link: "adjetivos.html",
+        logo: "",
+        logoColor: ""
+    },
+    {
+        level: 2,
+        lessonTitle: "4 Adjetivos",
+        lessonType: "explicacion",
+        id: "asdfasdasfasdf",
+        link: "adjetivos.html",
+        logo: "",
+        logoColor: ""
+    },
+    {
+        level: 3,
+        lessonTitle: "5 Adjetivos",
+        lessonType: "explicacion",
+        id: "asdfasdasfasdf",
+        link: "adjetivos.html",
+        logo: "",
+        logoColor: ""
+    },
+    {
+        level: 3,
+        lessonTitle: "6 Adjetivos",
+        lessonType: "explicacion",
+        id: "asdfasdasfasdf",
+        link: "adjetivos.html",
+        logo: "",
+        logoColor: ""
+    }];
 
-<li><a href="#!">sdfasdfdsa</a></li>
+    let html = `<li class="">
+                <div class="collapsible-header red lighten-5"><i class="material-icons">filter_drama</i>
+                 Nivel 1
+                </div>`;
 
-<li><div class="divider"></div></li>
-<li><a class="subheader">Subheader</a></li>
-<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>`
+lessons.forEach(function(item,i,array){
+    let level = item.level;
+    let title = item.lessonTitle;
+    let link = item.link;
+    html += `<div class="collapsible-body"><span class="valign-wrapper"><i class="material-icons tiny">chevron_right</i>${title}</span></div>`;
+    
+    
 
-let html1 = `    <li>
-<div class="collapsible-header"><i class="material-icons">filter_drama</i>Fisrst</div>
-<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-</li>
-<li><div class="divider"></div></li>
+    if((item.level!=array[i-1].level)){
+        html+= `</li>
 
-<li>
-<div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-</li>
-<li>
-<div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-</li>`
+                <div class="divider"></div>
 
-slideOut.innerHTML  = html1;
+                <li>
+                <div class="collapsible-header red lighten-5"><i class="material-icons">filter_drama</i>
+            Nivel ${item.level}
+        </div>`;
+        
+    }
+});
 
 
 
+slideOut.innerHTML  = html;
 }
