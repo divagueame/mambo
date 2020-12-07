e9bea48cf462af2b3c7967b74cb92271fe5ce02c
diva <martin.arce.teixeira@gmail.com>
2020-12-07 23:08:49 +0100

    Side bar module working

--- generateSidenav.js
+++ generateSidenav.js
@@ -3,61 +3,61 @@ const slideOut = document.querySelector("#slide-out");
 
 let lessons = [
     {
-        level: 1,
-        lessonTitle: "1 Sustantivos",
-        lessonType: "explicacion",
-        id: "cmaiwedsfasdfsdf",
-        link: "sustantivos.html",
-        logo: "",
-        logoColor: ""
+        'level': 1,
+        'lessonTitle': "1 Sustantivos",
+        'lessonType': "explicacion",
+        'id': "cmaiwedsfasdfsdf",
+        'link': "sustantivos.html",
+        'logo': "",
+        'logoColor': ""
     }
     ,{
-        level: 1,
-        lessonTitle: "2 Sustantivos 2",
-        lessonType: "actividad",
-        id: "asdfasdfasdf",
-        link: "actividadsustantivos.html",
-        logo: "",
-        logoColor: "blue"
+        'level': 1,
+        'lessonTitle': "2 Sustantivos 2",
+        'lessonType': "actividad",
+        'id': "asdfasdfasdf",
+        'link': "actividadsustantivos.html",
+        'logo': "",
+        'logoColor': "blue"
     },
     {
-        level: 2,
-        lessonTitle: "3 Adjetivos",
-        lessonType: "explicacion",
-        id: "asdfasdasfasdf",
-        link: "adjetivos.html",
-        logo: "",
-        logoColor: ""
+        'level': 2,
+        'lessonTitle': "3 Adjetivos",
+        'lessonType': "explicacion",
+        'id': "asdfasdasfasdf",
+        'link': "adjetivos.html",
+        'logo': "",
+        'logoColor': ""
     },
     {
-        level: 2,
-        lessonTitle: "4 Adjetivos",
-        lessonType: "explicacion",
-        id: "asdfasdasfasdf",
-        link: "adjetivos.html",
-        logo: "",
-        logoColor: ""
+        'level': 2,
+        'lessonTitle': "4 Adjetivos",
+        'lessonType': "explicacion",
+        'id': "asdfasdasfasdf",
+        'link': "adjetivos.html",
+        'logo': "",
+        'logoColor': ""
     },
     {
-        level: 3,
-        lessonTitle: "5 Adjetivos",
-        lessonType: "explicacion",
-        id: "asdfasdasfasdf",
-        link: "adjetivos.html",
-        logo: "",
-        logoColor: ""
+        'level': 3,
+        'lessonTitle': "5 Adjetivos",
+        'lessonType': "explicacion",
+        'id': "asdfasdasfasdf",
+        'link': "adjetivos.html",
+        'logo': "",
+        'logoColor': ""
     },
     {
-        level: 3,
-        lessonTitle: "6 Adjetivos",
-        lessonType: "explicacion",
-        id: "asdfasdasfasdf",
-        link: "adjetivos.html",
-        logo: "",
-        logoColor: ""
+        'level': 3,
+        'lessonTitle': "6 Adjetivos",
+        'lessonType': "explicacion",
+        'id': "asdfasdasfasdf",
+        'link': "adjetivos.html",
+        'logo': "",
+        'logoColor': ""
     }];
 
-    let html = `<li class="">
+    let html = `<li>
                 <div class="collapsible-header red lighten-5"><i class="material-icons">filter_drama</i>
                  Nivel 1
                 </div>`;
@@ -66,21 +66,22 @@ lessons.forEach(function(item,i,array){
     let level = item.level;
     let title = item.lessonTitle;
     let link = item.link;
-    html += `<div class="collapsible-body"><span class="valign-wrapper"><i class="material-icons tiny">chevron_right</i>${title}</span></div>`;
-    
-    
 
-    if((item.level!=array[i-1].level)){
+    
+    
+    if((i!=0)&&(level>array[i-1].level)){
+        console.log(level, "YES");
         html+= `</li>
+        <div class="divider"></div>
+        <li>
+        <div class="collapsible-header red lighten-5"><i class="material-icons">filter_drama</i>
+        Nivel ${item.level}
+        </div>`
+    } 
+    html += `<div class="collapsible-body"><span class="valign-wrapper"><i class="material-icons tiny">chevron_right</i>${title}</span></div>`;
 
-                <div class="divider"></div>
-
-                <li>
-                <div class="collapsible-header red lighten-5"><i class="material-icons">filter_drama</i>
-            Nivel ${item.level}
-        </div>`;
-        
-    }
+    
+    
 });
 
 
