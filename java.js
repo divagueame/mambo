cdf3d540fdb9240e16216dc484e72fed472c6c6d
diva <martin.arce.teixeira@gmail.com>
2020-12-08 07:08:21 +0100

    asd

--- java.js
+++ java.js
@@ -109,11 +109,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-  });
 
+  
 
 
 
