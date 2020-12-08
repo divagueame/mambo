92faa887872792d9edc14245a07ccdb8a8c4c09f
diva <martin.arce.teixeira@gmail.com>
2020-12-08 06:45:03 +0100

    as

--- java.js
+++ java.js
@@ -7,70 +7,70 @@ const exerciseContainer = document.querySelector("#exerciseContainer");
 
 
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// const auth = firebase.auth();
+// const db = firebase.firestore();
 
-function getUserId(){
-    firebase.auth().currentUser;
-}
+// function getUserId(){
+//     firebase.auth().currentUser;
+// }
 
 
 //Sign up form
-const signUpForm = document.querySelector("#signup-form");
-signUpForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const email = document.querySelector("#login-email").value;
-    const password = document.querySelector("#login-password").value;
-    auth.createUserWithEmailAndPassword(email,password).then((user)=>{
-        console.log("You are in!",user)
-    }).catch((error)=>{
-        console.log(error.code, error.message);
-        if(error.code=="auth/email-already-in-use"){
-            auth.signInWithEmailAndPassword(email,password).then((user)=>{
-                console.log("You already have and account and are in!",user)
-            }).catch((error)=>{
-                console.log(error.message);
-            })
-        }
-    })
-});
+// const signUpForm = document.querySelector("#signup-form");
+// signUpForm.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     const email = document.querySelector("#login-email").value;
+//     const password = document.querySelector("#login-password").value;
+//     auth.createUserWithEmailAndPassword(email,password).then((user)=>{
+//         console.log("You are in!",user)
+//     }).catch((error)=>{
+//         console.log(error.code, error.message);
+//         if(error.code=="auth/email-already-in-use"){
+//             auth.signInWithEmailAndPassword(email,password).then((user)=>{
+//                 console.log("You already have and account and are in!",user)
+//             }).catch((error)=>{
+//                 console.log(error.message);
+//             })
+//         }
+//     })
+// });
 
 
 
 // Log out
-const signOutButton = document.querySelector("#signOutButton");
-signOutButton.addEventListener('click', function(){
-    auth.signOut();
-    console.log("User has signed out")
-})
+// const signOutButton = document.querySelector("#signOutButton");
+// signOutButton.addEventListener('click', function(){
+//     auth.signOut();
+//     console.log("User has signed out")
+// })
 
 
 // On load, check if the user is logged in;
 
-document.addEventListener('DOMContentLoaded', (e)=>{
-    console.log("e")
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-        // User is signed in.
-            document.querySelectorAll(".loggedIn").forEach((e)=>{
-                e.style.display = ("block");
-            });
-            document.querySelectorAll(".loggedOut").forEach((e)=>{
-                e.style.display = ("none");
-            })
-            console.log("You're logged in")
-        } else {
-            console.log("You're NOT logged in");
-            document.querySelectorAll(".loggedOut").forEach((e)=>{
-                e.style.display = ("block");
-            });
-            document.querySelectorAll(".loggedIn").forEach((e)=>{
-                e.style.display = ("none");
-            })
-        // No user is signed in.
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', (e)=>{
+//     console.log("e")
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) {
+//         // User is signed in.
+//             document.querySelectorAll(".loggedIn").forEach((e)=>{
+//                 e.style.display = ("block");
+//             });
+//             document.querySelectorAll(".loggedOut").forEach((e)=>{
+//                 e.style.display = ("none");
+//             })
+//             console.log("You're logged in")
+//         } else {
+//             console.log("You're NOT logged in");
+//             document.querySelectorAll(".loggedOut").forEach((e)=>{
+//                 e.style.display = ("block");
+//             });
+//             document.querySelectorAll(".loggedIn").forEach((e)=>{
+//                 e.style.display = ("none");
+//             })
+//         // No user is signed in.
+//         }
+//     });
+// });
 
 
 
